4fe48ee0c6c341770cd760042db1e445ec96fbe6
King <king.hannibal@csedge.org>
2018-07-10 11:33:13 -0400

    i did lesson 3

--- change-machine.js
+++ change-machine.js
@@ -1,1 +1,8 @@
-// Author: FirstName LastName
+// Author: King Hannibal
+var readline = require("readline-sync");
+console.log("bLeep blOop! i'm a CHanGE macHINe!");
+var userName = readline.question("Please enter your name: ");
+console.log("Hello " + userName + "!");
+var cents = readline.question(" Please enter cents as a positive integer: ");
+console.log(cents + "cents make :");
+
