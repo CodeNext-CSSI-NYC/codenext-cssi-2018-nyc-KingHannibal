8a044e919b7b1d9bc29ed836c470c518192a9ba1
King <king.hannibal@csedge.org>
2018-07-11 17:04:30 -0400

    rock paper scissors sorta completed
    ;)

--- rps.js
+++ rps.js
@@ -1,9 +1,57 @@
 var readline = require("readline-sync");
 
-let random-int = require('random-int');
+let rock = 1 ;
 
-let compChoice = random-int(1 ,3);
+let paper = 2;
 
-if(compChoice == 1 ){
-	console.log()
+let scissors = 3;
+
+let randomInt = require('random-int');
+
+let compChoice = randomInt(1 ,3);
+
+let wannaPlay = readline.question(" Would you like to play a game of Rock-Paper-Scissors  (yes/no) : ");
+
+let userChoice;
+if(wannaPlay == "yes"){
+	userChoice = readline.question("Please choose Rock,paper or scissors: ");
+} else if(wannaPlay=="no"){
+	console.log("You whimp, go home!!");
+}
+else{
+	console.log("Thats not what i ask you");
+}
+
+//  lettting the computr know that "rock"( string ) is the same as rock variable which is aslo asign a #.
+if (userChoice == "rock"){
+ userChoice = rock ;
+} else if(userChoice == "paper"){
+	userChoice = paper ; 
+} else{
+userChoice = scissors ;
+}
+
+
+	// letting you know what the computer has chose!
+if (compChoice == 1 ){
+	console.log("the computer chose rock!" );
+} else if(compChoice == 2){
+	console.log("The computer chose paper!" );
+} else{
+	console.log("ther computer chose scissors");
+}
+
+// how you actually win
+// if you the computer choose rock
+if(compChoice == userChoice){
+	console.log("Great minds think alike! It was a TIE ");
+} else if(compChoice == 1 && userChoice == paper){
+	console.log(" You Fooled me, this time but i'll be back!! ");
+
+} else if (compChoice == 2 && userChoice == scissors){
+	console.log(" You Fooled me, this time but i'll be back!! ");
+} else if(compChoice == 3 && userChoice == rock){
+	console.log(" You Fooled me, this time but i'll be back!! ");
+} else{
+	console.log("I am Smarter than you,Thats why computers will take over!!!");
 }
